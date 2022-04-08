561dc42f0d24eeeb1a4d5819fb2feca04d326ad8
Matheu Plouffe <matheuplouffe@gmail.com>
2022-04-07 22:41:30 -0500

    Started adding structure for scheduler.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('./config/config');
 const express = require('express');
 const Twitter = require('twitter');
 const { generateTextTweet } = require('./text-tweet-generator/tweet-generator');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -13,20 +14,38 @@ const client = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+// STEP 1: Fetch schedule json
 setInterval(() => {
-    generateTextTweet()
-        .then((status) => {
-            console.log(status);
-            return client.post('statuses/update', { status: status });
-        })
-        .then((tweet) => {
-            console.log('Successful tweet!')
-        })
-        .catch((err) => {
-            console.log('ERR!: ', err);
-        });
-}, 28800000);
+    let schedule = getSchedule();
+    console.log(schedule);
+}, 5000);
+
+
+
+// setInterval(() => {
+//     generateTextTweet()
+//         .then((status) => {
+//             console.log(status);
+//             return client.post('statuses/update', { status: status });
+//         })
+//         .then((tweet) => {
+//             console.log('Successful tweet!')
+//         })
+//         .catch((err) => {
+//             console.log('ERR!: ', err);
+//         });
+// }, 28800000);
 
 app.get('/', (req, res) => res.send('Bot is running...'));
 
-app.listen(PORT, () => console.log(`Rnd Encounter up and running on ${PORT}`));
+app.listen(PORT, () => console.log(`Twitter Bot Scheduler is up and running on ${PORT}`));
+
+function getSchedule() {
+    try {
+        let rawSchedule = fs.readFileSync('./schedule/schedule.json', 'utf-8');
+        return JSON.parse(rawSchedule);
+    }
+    catch(err) {
+        console.log('Error reading schedule.json: ' + err);
+    }
+}
