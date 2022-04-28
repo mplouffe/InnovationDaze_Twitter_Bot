3d973834e338e9b69d9c219c246b5f1a8e811a1c
Matheu Plouffe <matheuplouffe@gmail.com>
2022-04-27 21:20:27 -0500

    Refactored collage generator.

--- bots/outArtBot/tools/collage_generator.js
+++ bots/outArtBot/tools/collage_generator.js
@@ -2,47 +2,54 @@ const Jimp = require('jimp');
 
 var imageBase;
 var imageArray = [];
+var numImagesToLoad;
+var imageGeneratorToRun;
+var imageGeneratedCallback;
 
-const fetchImage = () => {
+const generateCollageImage = (callback) => {
+    imageGeneratedCallback = callback;
+    imageArray = [];
+    numImagesToLoad = 3;
     new Jimp(500, 500, (err, image) => {
         imageBase = image;
     });
-    
-    Jimp.read('https://picsum.photos/500/500')
-        .then((image) => {
-            imageArray.push(image);
-            return Jimp.read('https://picsum.photos/500/500');
-        })
-        .then((image) => {
-            imageArray.push(image);
-            return Jimp.read('https://picsum.photos/500/500');
-        })
-        .then((image) => {
-            imageArray.push(image);
-
-            for (let i = 0; i < 5; i++) {
-                for (let j = 0; j < 5; j++) {
-                    let xPos = j * 100;
-                    let yPos = i * 100;
-
-                    let image = getRandomInt(3);
-                    let currentImage = imageArray[image].clone();
-                    
-                    currentImage.crop(xPos, yPos, 100, 100);
-
-                    imageBase.composite(currentImage, xPos, yPos);
-                }
-            }
-            console.log("about to output image...");
-            return imageBase.writeAsync('./artifacts/outArtBot/collage.png');
-        })
-        .then((result) => {
-            console.log("collage generated!");
-        });
+    imageGeneratorToRun = randomSquares;
+    loadImageArray();
 }
 
-const generateCollageImage = () => {
-    fetchImage();
+const loadImageArray = () => {
+    if (numImagesToLoad > 0) {
+        numImagesToLoad--;
+        Jimp.read('https://picsum.photos/500/500')
+            .then((image) => {
+                imageArray.push(image);
+                loadImageArray();
+            });
+    }
+    else
+    {
+        imageGeneratorToRun();
+        imageBase.writeAsync('./artifacts/outArtBot/collage.png')
+            .then((result, err) => {
+                console.log(err);
+            });
+    }
+}
+
+const randomSquares = () => {
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            let xPos = j * 100;
+            let yPos = i * 100;
+
+            let image = getRandomInt(3);
+            let currentImage = imageArray[image].clone();
+            
+            currentImage.crop(xPos, yPos, 100, 100);
+
+            imageBase.composite(currentImage, xPos, yPos);
+        }
+    }
 }
 
 const getRandomInt = (max) => {
